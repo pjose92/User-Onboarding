96c5dfd4fab9e476f8eb617e5b540ad23522f446
Jose <pjose14.jp38@yahoo.com>
2020-02-22 17:29:58 -0600

    completed mvp. did some styling

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Form from "./component/Form";
+
+function App() {
+  return (
+    <div className="grid" style={{ height: "100vh", paddingTop: 30 }}>
+    <Form />
+  </div>
+  );
+}
+
+export default App;
